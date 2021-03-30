a6f099f2865829c9b69ede1eda491b7844527fb4
unknown <pritampaul9798@gmail.com>
2021-03-31 00:19:29 +0530

    employee wage computation conditional

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,11 +1,20 @@
 import java.util.*;
-public class uc5_EmployeeWage_Computation_Using_Switch_Case{
+public class uc6_EmployeeWage_Computation_conditional{
            
            
        public static void main(String[] args){
-           int hrs=0;
+           int hrs= 0;
            int wage;
-           int rate=20;
+           int rate= 20;
+           int totalwage= 0;
+           int workingdays=0;
+           int max_days= 20;
+           int total_emphrs = 0;
+           int max_hrs=100;
+           while ( total_emphrs <= max_hrs && workingdays < max_days ) {
+               int emp_hrs = 0;
+               workingdays++;
+               
             Random rnum = new Random();
             int checker = rnum.nextInt(3);
             switch (checker) {
@@ -13,21 +22,22 @@ public class uc5_EmployeeWage_Computation_Using_Switch_Case{
           
               case 1:
                   hrs = 8;
-                  wage= hrs*rate;
-                  System.out.println("the employee wage for full time employee is ");
-                  System.out.println(wage);
                   break;
+                  
               case 0:
                   hrs=4;
-                   wage= hrs*rate;
-                   System.out.println("the employee wage for part time employee is ");
-                  System.out.println(wage);
                   break;
+                  
               default:
-              
-              System.out.println("the employee is absent ");
-          }
+                  hrs=0;
+          
+                 }  
+           wage = hrs * rate;
+            totalwage += wage ;
+            System.out.println(wage);
+           }
+         System.out.println(totalwage);
             
         
-         }
-      }
+    }
+ }

3ce55442efda5d44c241f6d91e64538731a9bf0b
unknown <pritampaul9798@gmail.com>
2021-03-31 00:16:47 +0530

    employee wage computation using switch case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,38 +1,33 @@
 import java.util.*;
-public class uc4_EmployeeWage_Computation_for_A_month{
+public class uc5_EmployeeWage_Computation_Using_Switch_Case{
            
            
        public static void main(String[] args){
            int hrs=0;
            int wage;
            int rate=20;
-           int totalwage=0;
-            for (int day=0; day<20; day++)
-            {
             Random rnum = new Random();
             int checker = rnum.nextInt(3);
-           
             switch (checker) {
             
           
               case 1:
                   hrs = 8;
+                  wage= hrs*rate;
+                  System.out.println("the employee wage for full time employee is ");
+                  System.out.println(wage);
                   break;
-                  
               case 0:
                   hrs=4;
+                   wage= hrs*rate;
+                   System.out.println("the employee wage for part time employee is ");
+                  System.out.println(wage);
                   break;
-                  
               default:
-                  hrs=0;
-          
-                 }  
-           wage = hrs * rate;
-            totalwage += wage ;
-            System.out.println(wage);
-        }
-         System.out.println(totalwage);
+              
+              System.out.println("the employee is absent ");
+          }
             
         
-    }
- }
+         }
+      }

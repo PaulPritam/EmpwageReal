a5d7ec750947cb6377bb75f038b83e7c012277a5
unknown <pritampaul9798@gmail.com>
2021-03-31 00:10:38 +0530

    wage computation with part time employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,19 +1,25 @@
 import java.util.*;
-public class uc2_EmployeeWage_Computation{
+public class uc3_EmployeeWage_Computation_with_Part_Time_Employee{
        public static void main(String[] args){
            int wage;
             Random rnum = new Random();
-            int num = rnum.nextInt(2);
+            int num = rnum.nextInt(3);
             System.out.println(num);
-          if ( num == 1)
+          if ( num == 0)
           {
               wage = 8*20;
-              System.out.println("the employee wage per day is ");
+              System.out.println("the employee wage is ");
+              System.out.println(wage);
+          }
+          else if ( num == 1) 
+          {
+              wage = 4*20;
+              System.out.println("wage of the part time employee is ");
               System.out.println(wage);
           }
           else 
           {
-              System.out.println("the employee wage is 0");
+              System.out.println("the employee is absent");
           }
-          }
-       }
+         }
+      }
